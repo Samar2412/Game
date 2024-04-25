30693eeeb9d086ca750281705d529a0c949b9835
Samar2412 <164082883+Samar2412@users.noreply.github.com>
2024-04-25 21:01:15 +0530

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,3 @@
+document.body.style.background = '#4CA1AF';
+document.body.style.background = '-webkit-linear-gradient(to left, #4CA1AF , #C4E0E5)';
+document.body.style.background = 'linear-gradient(to left, #4CA1AF , #C4E0E5)';
